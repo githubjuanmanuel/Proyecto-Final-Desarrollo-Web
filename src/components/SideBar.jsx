684115f65e6cd97a545b20c49faacddd9799d98f
Carlos <githubjuanmanuel@gmail.com>
2024-06-15 13:10:48 -0500

    cambios

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Home from "../routes/Home";
-import NewTask from "../routes/NewTask";
-import ViewTasks from "../routes/ViewTasks";
 
 function SideBar() {
   return (
